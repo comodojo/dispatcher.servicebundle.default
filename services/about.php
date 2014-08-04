55d7f6a13451bc348e75435c65f66a83e7c081ff
comodojo <info@comodojo.org>
2014-08-04 21:40:50 +0200

    Tests adapted for STANDARD mode

--- services/about.php
+++ services/about.php
@@ -14,13 +14,45 @@ class about extends Service {
 
         $template = new TemplateBootstrap("navbar");
 
-        $template->setTitle("Comodojo dispatcher")->setBrand("comodojo/dispatcher");
+        if ( DISPATCHER_USE_REWRITE ) {
+
+            $test_link  = DISPATCHER_BASEURL.'test/';
+
+            $about_link = DISPATCHER_BASEURL.'about/';
+
+        } else {
+
+            $test_link  = DISPATCHER_BASEURL.'?service=test';
+
+            $about_link = DISPATCHER_BASEURL.'?service=about';
+
+        }
+
+        $template->setTitle("Comodojo dispatcher")->setBrand("comodojo::dispatcher");
 
         $template->addMenu("right")
-                 ->addMenuItem("Test", DISPATCHER_BASEURL."test/", "right")
-                 ->addMenuItem("About", DISPATCHER_BASEURL."about/", "right");
+                 ->addMenuItem("Test", $test_link, "right")
+                 ->addMenuItem("About", $about_link, "right");
 
-        $template->setContent("<h1>About Content Here</h1>");
+        $content = '<div class="container"><h1>Comodojo dispatcher</h1><h3 class="text-muted">service-oriented REST microframework</span></h3>';
+
+        $content .= '<p style="margin: 40px; text-align: center">
+                <a href="http://docs.comodojo.org/dispatcher/" target="_blank" class="btn btn-lg btn-success" style="margin-top:40px;" role="button"><span class="glyphicon glyphicon-book"></span>&nbsp;&nbsp;Read documentation</a>&nbsp;&nbsp;
+                <a href="http://api.comodojo.org/dispatcher/" target="_blank" class="btn btn-lg btn-primary" style="margin-top:40px;" role="button"><span class="glyphicon glyphicon-cog"></span>&nbsp;&nbsp;Explore API</a>&nbsp;&nbsp;
+                <a href="https://github.com/comodojo/dispatcher.framework" target="_blank" class="btn btn-lg btn-danger" style="margin-top:40px;" role="button"><span class="glyphicon glyphicon-new-window"></span>&nbsp;&nbsp;Fork on GitHub</a>
+            </p>';
+
+        $content .= '<section style="margin-top:60px;" class="jumbotron">
+                    <div class="section-inner">
+                        <p>Comodojo dispatcher is distributed under the terms of the GNU V3 General Public License as published by the Free Software Foundation; you can find a copy in each package released or following <a href="http://www.gnu.org/copyleft/gpl.html" target="_blank">this link</a>.</p>
+                        <address>
+                            Keep in touch: <a href="mailto:#">info@comodojo.org</a><br />
+                        </address>
+                    </div>
+                </section>            
+            </div>';
+
+        $template->setContent($content);
 
         return $template->serialize();
 
--- services/landing.php
+++ services/landing.php
@@ -14,21 +14,35 @@ class landing extends Service {
 
         $template = new TemplateBootstrap("navbar");
 
+        if ( DISPATCHER_USE_REWRITE ) {
+
+            $test_link  = DISPATCHER_BASEURL.'test/';
+
+            $about_link = DISPATCHER_BASEURL.'about/';
+
+        } else {
+
+            $test_link  = DISPATCHER_BASEURL.'?service=test';
+
+            $about_link = DISPATCHER_BASEURL.'?service=about';
+
+        }
+
         $content = '
             <div class="jumbotron">
-                <h1>Comodojo dispatcher</h1>
-                <p>Welcome to dispatcher. This is a default landing page (part of dispatcher.servicebundle.default).</p>
-                <p>To jump to test section use the button below or top navbar</p>
+                <h1>Welcome to Comodojo dispatcher</h1>
+                <p>This is a default landing page (provided by dispatcher.servicebundle.default).</p>
+                <p>To jump to test section use the fat button below or top navbar</p>
                 <p>
-                    <a class="btn btn-lg btn-primary" href="'.DISPATCHER_BASEURL.'test/" role="button">Begin tests</a>
+                    <a class="btn btn-lg btn-primary" href="'.$test_link.'" role="button">Begin tests</a>
                 </p>
             </div>';
 
-        $template->setTitle("Comodojo dispatcher")->setBrand("comodojo/dispatcher");
+        $template->setTitle("Comodojo dispatcher")->setBrand("comodojo::dispatcher");
 
         $template->addMenu("right")
-                 ->addMenuItem("Test", DISPATCHER_BASEURL."test/", "right")
-                 ->addMenuItem("About", DISPATCHER_BASEURL."about/", "right");
+                 ->addMenuItem("Test", $test_link, "right")
+                 ->addMenuItem("About", $about_link, "right");
 
         $template->setContent($content);
 
